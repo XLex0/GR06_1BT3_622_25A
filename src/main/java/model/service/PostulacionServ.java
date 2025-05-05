5c1efeb56ee132a0c5f9caa8739613df1e5482a1
Berserker758 <pablo.maldonado@epn.edu.ec>
2025-05-04 20:42:43 -0500

    Revert "Actualizacion"
    This reverts commit dd6104d6b1020f9f63472fb55003611a465e09ec.

--- src/main/java/model/service/PostulacionServ.java
+++ src/main/java/model/service/PostulacionServ.java
@@ -1,47 +1,16 @@
 package model.service;
 
 import model.dao.PostulacionDAO;
-import model.dao.TicketDAO;
-import model.dao.UsuarioDAO;
 import model.entities.Postulacion;
-import model.entities.Ticket;
-import model.entities.Usuario;
-import model.factory.DAOFactoria;
 
 import java.util.List;
 
 public class PostulacionServ {
-    private PostulacionDAO postulacionDAO = new DAOFactoria().obtenerPostulacionDAO();
-    private TicketDAO ticketDAO = new DAOFactoria().obtenerTicketDAO();
-    private UsuarioDAO usuarioDAO = new DAOFactoria().obtenerUsuarioDAO();
+    private PostulacionDAO postulacionDAO = new PostulacionDAO();
 
-    // Método refactorizado que maneja la lógica de negocio
-    public boolean registrarPostulacion(Long ticketId, Long paseadorId) {
-        try {
-            Ticket ticket = ticketDAO.findById(ticketId);
-            Usuario paseador = usuarioDAO.findById(paseadorId);
-
-            if (ticket != null && paseador != null) {
-                Postulacion postulacion = new Postulacion();
-                postulacion.setFecha(new java.sql.Date(System.currentTimeMillis()));
-                postulacion.setTicket(ticket);
-                postulacion.setUsuario(paseador);
-                postulacion.setAprobado(false);
-
-                postulacionDAO.registrarPostulacion(postulacion);
-                return true;
-            }
-
-            return false;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-
-    // Eliminar una postulación por ID
-    public void eliminarPostulacion(Long id) {
-        postulacionDAO.eliminarPostulacion(id);
+    // Registrar una nueva postulación
+    public void registrarPostulacion(Postulacion postulacion) {
+        postulacionDAO.registrarPostulacion(postulacion);
     }
 
     // Listar todas las postulaciones
@@ -49,8 +18,11 @@ public class PostulacionServ {
         return postulacionDAO.listarPostulaciones();
     }
 
-    // Buscar una postulación específica
-    public Postulacion listarPostulacion(Long id) {
-        return postulacionDAO.listarPostulacion(id);
+    public void aceptarPostulacion(Long id) {
+        postulacionDAO.aceptarPostulacion(id);
+    }
+
+    public List<Postulacion> listarPostulacionesPorCliente(Long clienteId) {
+        return postulacionDAO.listarPostulacionesPorCliente(clienteId);
     }
 }
